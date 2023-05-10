08eb0a565ac4b2da7fa267bd985c42b3d48349c4
Stefan McCabe <stefanmccabe@gmail.com>
2023-05-10 16:51:36 -0400

    make script order explicit

--- replication.sh
+++ replication.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-Rscript src/anes_scratch.R
-Rscript src/regression_scratch.R
+Rscript src/01-process_anes_data.R
+Rscript src/02-run_models.R
 
 cd results/appendix
 latexmk --xelatex latex_appendix.tex
