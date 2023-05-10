2b5c3e85125b19949972420d88aefb4f1b32eda5
Stefan McCabe <stefanmccabe@gmail.com>
2023-05-10 18:09:41 -0400

    log R/latex output

--- replication.sh
+++ replication.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-Rscript src/01-process_anes_data.R
-Rscript src/02-run_models.R
+echo "Processing ANES data..." | tee run.log
+Rscript src/01-process_anes_data.R 2>&1 | tee -a run.log
+echo "Fitting models..." | tee -a run.log
+Rscript src/02-run_models.R 2>&1 | tee -a run.log
 
 cd results/appendix
-latexmk --xelatex latex_appendix.tex
+echo "Compiling appendix..." | tee -a ../../run.log
+latexmk --xelatex latex_appendix.tex 2>&1 | tee -a ../../run.log
 cd ../..
